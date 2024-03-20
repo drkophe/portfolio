52ab3542a1e6ef69ff94ae7992031c719b6f304e
kophe <lorenzo.bosi.pro@gmail.com>
2024-03-20 12:00:43 +0100

    Update Navbar.jsx
    change classname into clsx

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 import Button from "../Button";
 import avatar from "../../assets/img/avatar.png";
+import clsx from "clsx";
 
 export default function Navbar() {
     // state
     const [links, setLinks] = useState(['Acceuil', 'Projects', 'Skills', 'Work Together']);
 
+    // const [mode, setMode] = useState('light');
+
     const navigation = links.map((link) => 
     <li key={link} className="mx-3 text-white">
         <a href="#" 
-        className="block relative px-1 
-        after:content-[''] 
-        after:absolute 
-        after:bottom-0 
-        after:left-0 
-        after:w-full 
-        after:h-0.5 
-        after:bg-indigo-400 
-        after:opacity-0 
-        after:transition-all 
-        after:duration-300 
-        
-        hover:after:opacity-100 
-        hover:after:translate-y-1 
-        focus:after:opacity-100
-        focus:after:translate-y-1">
+        className={clsx(
+            // Spacing & Position    
+            " block relative px-1",
+            // After
+                // Position
+                "after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 ",
+                // Content & Background
+                " after:content-[''] after:bg-indigo-400 after:opacity-0",
+                // Animation
+                " after:transition-all after:duration-300 ",
+            // Hover
+                "hover:after:opacity-100 hover:after:translate-y-1 ",
+            // Focus
+                "focus:after:opacity-100 focus:after:translate-y-1 ",
+        )}>
             {link}
         </a>
     </li>
